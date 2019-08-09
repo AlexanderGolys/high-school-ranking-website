6490c26354938a95fa2c86027ebca52359abb089
AlexanderGolys <szyba71830>
2019-08-09 19:35:26 +0200

    added settings for apache

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path='/var/www/mysite'
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+if path not in sys.path:
+  sys.path.append(path)
 
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
