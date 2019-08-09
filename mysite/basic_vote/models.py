9c628d1da789063e91e4e354cc111bf2040875f2
AlexanderGolys <szyba71830>
2019-08-09 19:34:42 +0200

    gitignored updated

--- mysite/basic_vote/models.py
+++ mysite/basic_vote/models.py
@@ -27,7 +27,6 @@ class School(models.Model):
     name = models.CharField(max_length=1000)
 
 
-
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField()
