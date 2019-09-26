cb8f3c4588f92e2ad5bf56b7fc4cf362c8c90017
AlexanderGolys <szyba71830>
2019-09-26 11:10:57 +0200

    deleted db to merge

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'social_django',
+    'basic_vote',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
-    'basic_vote',
+
 ]
 
 MIDDLEWARE = [
