9d9530baddcc164407ec6994876b147f106a97ac
Michał Rokita <mrokita@mrokita.pl>
2019-09-30 16:23:54 +0200

    Gitignore + statyki

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+FILE_SERVING_ROOT = os.path.join(BASE_DIR, 'public')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(FILE_SERVING_ROOT, 'auto_collected/')
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -143,5 +144,3 @@ SESSION_COOKIE_AGE = 86400 # sec
 SESSION_COOKIE_DOMAIN = None
 SESSION_COOKIE_NAME = 'DSESSIONID'
 SESSION_COOKIE_SECURE = False
-
-STATIC_ROOT = './basic_vote/static'
